6ce97c21f6f88af557792fffaa7e7674dc484c92
Ayush <ayush.chakladar@gmail.com>
2021-08-15 17:12:24 +0530

    text field decoration constant

--- lib/constants.dart
+++ lib/constants.dart
@@ -19,3 +19,19 @@ const kMessageContainerDecoration = BoxDecoration(
 );
 
 const kBackgroundColor = Colors.white10 ;
+
+const kTextFieldDecoration=InputDecoration(
+  contentPadding:
+  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
