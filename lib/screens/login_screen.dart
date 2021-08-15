3c900032f45dde00d3c8288af7ab1b2f64c9661f
Ayush <ayush.chakladar@gmail.com>
2021-08-15 17:05:37 +0530

    rounded button componnet

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flash_chat/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flash_chat/components/rounded_button.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
@@ -80,24 +81,12 @@ class _LoginScreenState extends State<LoginScreen> {
             SizedBox(
               height: 24.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Implement login functionality.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
+        RoundedButton(
+            title: 'Log in',
+            colour: Colors.lightBlueAccent,
+            onPressed: (){
+
+        }),
           ],
         ),
       ),
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flash_chat/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flash_chat/components/rounded_button.dart';
 
 class RegistrationScreen extends StatefulWidget {
 
@@ -77,25 +78,12 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             SizedBox(
               height: 24.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Implement registration functionality.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-            ),
+         RoundedButton(
+             title: 'Register',
+             colour: Colors.blueAccent,
+             onPressed: (){
+
+            }),
           ],
         ),
       ),
